c1b35bf1790336271dd66b5c37d42e0641f7b33f
coder <>
2025-11-07 18:03:09 -0500

    login stuff

--- app/models.py
+++ app/models.py
@@ -4,9 +4,10 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from flask_login import UserMixin
+from app import login
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -30,3 +31,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return "<Posts {}>".format(self.body)
+    
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 @app.route('/')
 @app.route('/index')
 def index():
@@ -19,8 +23,9 @@ def index():
 
 @app.route("/login", methods= ["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash ("Login requested for user {} , remember me = {} ".format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
     return render_template("login.html", title="Sign In", form=form)
