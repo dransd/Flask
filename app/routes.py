986490ee89f0a2d88b203cb2aae3653a5a11b318
coder <>
2025-11-14 18:01:27 -0500

    Login/logout

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -28,4 +28,14 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember= form.remember_me.data)
+        return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
