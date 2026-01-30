adc3a391283ef9232ec7f5bca194987795ebd2af
coder <>
2026-01-30 17:59:59 -0500

    Profiles

--- app/routes.py
+++ app/routes.py
@@ -59,3 +59,13 @@ def register():
         flash("You are now a user")
         return redirect(url_for("login"))
     return render_template("register.html", title="register", form=form)
+
+@app.route("/user/<username>")
+@login_required
+def user (username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {"author": user,"body": "test post #1"}, 
+        {"author": user,"body": "test post #2"}
+    ]
+    return render_template("user.html", user = user, posts = posts)
